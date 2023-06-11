b3017de37f1e73868219ebf427e54985c51d89d3
Víctor Ramón Rodríguez Domínguez <5773902+vrdominguez@users.noreply.github.com>
2023-06-11 21:56:02 +0200

    Added logs with sirupsen/logrus

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
 # dinaIP GO
 
-Modern version of dinaIP client written in go.
+This is an updated version of the dinaIP client written in Go, developed as an opportunity to explore and experiment with Golang.
+
+## Configuration
+
+To configure the dinaIP GO client, you need to create a YAML file with the following format:
+
+```yaml
+username: userDinahostingAPI
+password: passwordDinahostingAPI
+
+logs:
+  path: /path/to/file.log
+  level: DEBUG
+
+zones:
+  mydomain.tld:
+    - mysubdomain1
+    - mysubdomain2
+  mydomain2.tld:
+    - subdomain
+```
+
+## Example system.d configuration file
+
+```systemd
+[Unit]
+Description=dinaIP GO
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=systemUserForDinaIP
+Group=systemUserForDinaIP
+ExecStart=/path/to/dinapi-go -c /path/to/config.yaml
+
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Make sure to replace userDinahostingAPI and passwordDinahostingAPI with your actual username and password for the Dinahosting API. Additionally, specify the log file path and the desired logging level. Lastly, provide the domains and their respective subdomains that you want to manage.
+
+## Use it
+
+Before running the dinaIP GO client, ensure that you have a valid configuration file with the correct values, including a log file path where the program can write logs.
+
+### Runing from source
+
+1. Clone this repository.
+2. Open a terminal and navigate to the directory where the program is located.
+3. Run the code with the command `go run ./main.go -c /path/to/config.yaml`, replacing /path/to/config.yaml with the path to your configuration file.
+
+### Running in the Command Line
+
+1. Download the compiled program from the [lastest release](https://github.com/vrdominguez/dinaip-go/releases/latest) on the [project's GitHub page](https://github.com/vrdominguez/dinaip-go/releases) or compile it yourself.
+2. Open a terminal and navigate to the directory where the program is located.
+3. Execute the command `./dinapi-go -c /path/to/config.yaml`, replacing /path/to/config.yaml with the path to your configuration file.
+
+### Running as a Service (with systemd)
+
+1. Download the compiled program from the [lastest release](https://github.com/vrdominguez/dinaip-go/releases/latest) on the [project's GitHub page](https://github.com/vrdominguez/dinaip-go/releases) or compile it yourself.
+2. Move the binary to `/opt/dinaip-go/` or other path of your choice.
+3. Create a yaml configuration for the program. (I placed mine at `/etc/dinaIP.yaml`)
+4. If you don't want to run the service a root (yo may no run it as root), crete an user and a group for it.
+5. Create your system.d file from example on this README.
+6. Save the unit file with a .service extension (e.g., `dinaip-go.service`).
+7. Move the unit file to the appropriate location for systemd unit files (e.g., `/etc/systemd/system/`).
+8. Reload the systemd daemon to load the new unit file: `sudo systemctl daemon-reload`.
+9. Enable the service to start on boot: `sudo systemctl enable dinaip-go`.
+10. Start the service: `sudo systemctl start dinaip-go`.
+11. Verify that the service is running: `sudo systemctl status dinaip-go`.
+
+Please note that you may need to adjust the unit file and paths according to your specific setup and requirements.
--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/vrdominguez/dinaip-go/config"
 	"github.com/vrdominguez/dinaip-go/dh_api"
@@ -18,6 +22,13 @@ var configuration *config.Config
 var client *dh_api.DinahostingClient
 
 func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Before config set logs to info via STDOUT
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+
 	// Load config
 	var configFile string
 	flag.StringVar(&configFile, "c", "", "path to config file")
@@ -28,12 +39,26 @@ func init() {
 	}
 
 	if config, err := config.ReadConfig(configFile); err != nil {
-		panic(err)
+		log.Panicf("Cannot read config file at %s", configFile)
 	} else {
 		configuration = config
 	}
 
-	// Set client
+	// Set file log
+	if file, err := os.OpenFile(configuration.Logs.Path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666); err != nil {
+		log.Panicf("Cannot create logs file at %s: %s", configuration.Logs.Path, err)
+	} else {
+		log.Infof("Logs changed to file: %s", configuration.Logs.Path)
+		log.SetOutput(file)
+	}
+
+	// Update loglevel with config value
+	if logLevel, err := convertLogLevel(configuration.Logs.Level); err != nil {
+		log.Panicf("Cannot set log level: %s", err)
+	} else {
+		log.SetLevel(logLevel)
+	}
+
 	client = dh_api.NewClient(configuration.Username, configuration.Password)
 }
 
@@ -43,50 +68,90 @@ func main() {
 	for {
 		currentIp, err := iputils.GetPublicIp()
 		if err != nil {
-			panic(err)
-		}
-
-		for domain, subdomains := range configuration.Zones {
-			// Get domain records
-			recordsResponse, err := client.GetTypeIpRecords(domain, dh_api.TypeA)
-			if err != nil {
-				fmt.Printf("Cannot check domain %s: %s\n", domain, err)
-				continue // Go for next domain if zones cannot be checked
-			}
-
-			for _, subdomain := range subdomains {
-				subdomainIp := ""
-
-				for _, dnsRecord := range recordsResponse.Data {
-					if dnsRecord.Hostname == subdomain {
-						subdomainIp = dnsRecord.Ip
-						break
-					}
+			log.Errorf("Cannot get current IP: %s", err)
+		} else {
+			for domain, subdomains := range configuration.Zones {
+				// Get domain records
+				recordsResponse, err := client.GetTypeIpRecords(domain, dh_api.TypeA)
+				if err != nil {
+					log.Errorf("Cannot check domain %s: %s", domain, err)
+					continue // Go for next domain if zones cannot be checked
 				}
 
-				if subdomainIp == "" {
-					fmt.Printf("Domain: %s, subdomain: %s, Create new Zone\n", domain, subdomain)
-					if err := client.SetTypeIpRecord(domain, subdomain, currentIp, "", dh_api.TypeA); err != nil {
-						fmt.Printf("ERROR: Cannot create %s.%s with ip %s: %s\n", domain, subdomain, currentIp, err)
-					} else {
-						fmt.Printf("Updated %s.%s with ip %s\n", domain, subdomain, currentIp)
+				for _, subdomain := range subdomains {
+					logFields := convertToLogrusFields(map[string]string{
+						"domain":  domain,
+						subdomain: subdomain,
+					})
+					subdomainLogger := log.WithFields(logFields)
+
+					subdomainLogger.Debugf("Working with %s.%s", subdomain, domain)
+
+					subdomainIp := ""
+					for _, dnsRecord := range recordsResponse.Data {
+						if dnsRecord.Hostname == subdomain {
+							subdomainIp = dnsRecord.Ip
+							break
+						}
 					}
 
-				} else if currentIp != subdomainIp {
-					fmt.Printf("Domain: %s, subdomain: %s, Update IP\n", domain, subdomain)
-					if err := client.SetTypeIpRecord(domain, subdomain, currentIp, subdomainIp, dh_api.TypeA); err != nil {
-						fmt.Printf("ERROR: Cannot update %s.%s from ip %s to %s: %s\n", domain, subdomain, subdomainIp, currentIp, err)
-					} else {
-						fmt.Printf("Updated %s.%s with ip %s\n", subdomain, domain, currentIp)
-					}
+					subdomainLogger.Debugf("Current IP %s", subdomainIp)
+					if subdomainIp == "" {
+						subdomainLogger.Infof("Creating new dns record")
+						if err := client.SetTypeIpRecord(domain, subdomain, currentIp, "", dh_api.TypeA); err != nil {
+							subdomainLogger.Errorf("Cannot create zone: %s", err)
+						} else {
+							subdomainLogger.Info("Create new zone")
+							subdomainLogger.Debugf("New ip: %s", currentIp)
+						}
 
-				} else {
-					fmt.Printf("%s.%s already has the ip %s\n", subdomain, domain, currentIp)
+					} else if currentIp != subdomainIp {
+						subdomainLogger.Infof("Updating dns record")
+						if err := client.SetTypeIpRecord(domain, subdomain, currentIp, subdomainIp, dh_api.TypeA); err != nil {
+							subdomainLogger.Errorf("Cannot update zone record: %s", err)
+						} else {
+							subdomainLogger.Info("Updated zone recod")
+							subdomainLogger.Debugf("New ip: %s", currentIp)
+						}
+
+					} else {
+						subdomainLogger.Info("No updates needed")
+					}
 				}
 			}
 		}
 
 		// Wait 5 minutes...
+		log.Debug("Waiting for 5 minutes until next review")
 		time.Sleep(5 * time.Minute)
 	}
+}
+
+func convertLogLevel(logLevel string) (log.Level, error) {
+	switch strings.ToUpper(logLevel) {
+	case "DEBUG":
+		return log.DebugLevel, nil
+	case "INFO":
+		return log.InfoLevel, nil
+	case "WARN", "WARNING":
+		return log.WarnLevel, nil
+	case "ERROR":
+		return log.ErrorLevel, nil
+	case "FATAL":
+		return log.FatalLevel, nil
+	case "PANIC":
+		return log.PanicLevel, nil
+	default:
+		return log.InfoLevel, fmt.Errorf("invalid log level: %s", logLevel)
+	}
+}
+
+func convertToLogrusFields(fields map[string]string) log.Fields {
+	logFields := log.Fields{}
+
+	for key, value := range fields {
+		logFields[key] = value
+	}
+
+	return logFields
 }
